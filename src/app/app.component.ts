6e0ec27871cf60210756f08bbe8e443a5caf005c
amosmoyo <amosmoyo5300@gmail.com>
2020-02-17 17:30:01 +0300

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,28 +3,56 @@ import { Posts } from './posts';
 import { AuthetificationService } from './authetification.service';
 import { ServerHistoryService } from './server-history.service';
 
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
 })
 export class AppComponent implements OnInit {
   title = 'projectThree';
 
   posts: Posts[] = [];
 
+  isRoot: boolean;
+
   previousRoute: string;
 
   constructor(
     private auth: AuthetificationService,
-    private serverhistrory: ServerHistoryService
+    // private serverhistrory: ServerHistoryService,
+    private location: Location,
+    private router: Router
   ) {
-    serverhistrory.loadRouting();
+    // serverhistrory.loadRouting();
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  goForward() {
+    this.location.forward();
+  }
+
+  getPath() {
+    console.log(this.location.path());
   }
 
   ngOnInit() {
     this.auth.autoAutheticate();
-    this.previousRoute = this.serverhistrory.getPreviousUrl();
+    // this.previousRoute = this.serverhistrory.getPreviousUrl();
+
+    this.router.events.subscribe(event => {
+      if (this.location.path() !== '') {
+        this.isRoot = false;
+      } else {
+        this.isRoot = true;
+      }
+    });
   }
 
   onEmitValue(data) {
