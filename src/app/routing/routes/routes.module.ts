71c4dec041d8e93db57e14813216ddd6e2509dd3
amosmoyo <amosmoyo5300@gmail.com>
2020-02-17 20:20:50 +0300

    new changes

--- src/app/routing/routes/routes.module.ts
+++ src/app/routing/routes/routes.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
   {path: 'edit/:id', component: EditComponent, canActivate: [AuthGurds]},
   {path: 'create', component: CreateComponent, canActivate: [AuthGurds]},
   {path: 'auth', loadChildren: 'src/app/users/user/user.module#UserModule'}
+  {path: '**', component: HomeComponent}
 ];
 
 @NgModule({
